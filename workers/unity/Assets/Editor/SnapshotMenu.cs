aeeccea8c0feb375da51ed4b9ecbd388a9a4d6b8
Giovanni Colucci <g.colucci13@stuenti.unisa.it>
2017-10-27 15:44:14 +0200

    Initial commit

---
+++ workers/unity/Assets/Editor/SnapshotMenu.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Gamelogic.Core;
+using Improbable;
+using Improbable.Worker;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor 
+{
+	public class SnapshotMenu : MonoBehaviour
+	{
+		[MenuItem("Improbable/Snapshots/Generate Default Snapshot")]
+		[UsedImplicitly]
+		private static void GenerateDefaultSnapshot()
+		{
+			var snapshotEntities = new Dictionary<EntityId, Entity>();
+
+			// Add entity data to the snapshot
+
+			SaveSnapshot(snapshotEntities);
+		}
+
+		private static void SaveSnapshot(IDictionary<EntityId, Entity> snapshotEntities)
+		{
+			File.Delete(SimulationSettings.DefaultSnapshotPath);
+			var maybeError = Snapshot.Save(SimulationSettings.DefaultSnapshotPath, snapshotEntities);
+
+			if (maybeError.HasValue)
+			{
+				Debug.LogErrorFormat("Failed to generate initial world snapshot: {0}", maybeError.Value);
+			}
+			else
+			{
+				Debug.LogFormat("Successfully generated initial world snapshot at {0}", SimulationSettings.DefaultSnapshotPath);
+			}
+		}
+	}
+}
